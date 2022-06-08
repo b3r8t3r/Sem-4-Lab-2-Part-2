ff413d1ef8cafce5a6e0ee7b888facf28e84b28b
Black_Wolfy007 <shuvaev.v.6@gmail.com>
2022-06-08 22:13:35 +0700

    Добавьте файлы проекта.

---
+++ Sem 4 Lab 2 Part 2/Sem 4 Lab 2 Part 2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+string Read() {
+    ifstream in("input.txt");
+    string txt;
+    getline(in, txt);
+    return txt;
+}
+
+void Write(string txt) {
+    ofstream out("output.txt");
+    out << txt;
+}
+
+void Analize(string txt) {
+    
+    vector<int> id;
+    string result;
+    for (int i = 0; i < txt.size(); i++) {
+        if (txt[i] == '#') id.push_back(i);
+    }
+    
+    /*if ((fmod((int)id.size(), 2) == 0) && ((int)id.size())) out << "Everything is fine" << endl << txt.substr(id[0] + 1, id[1] - id[0] - 1) << endl;
+    else out << "Everything is not fine" << endl;*/
+    if ((fmod((int)id.size(), 2) == 0) && ((int)id.size())) {
+        result = "Everything is fine";
+    }
+    else {
+        result = "Everything is not fine";
+    }
+
+    Write(result);
+}
+
+
+
+int main()
+{
+    string text = Read();
+    Analize(text);
+}
